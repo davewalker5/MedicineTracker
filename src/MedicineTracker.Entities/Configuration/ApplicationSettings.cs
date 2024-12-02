4cf491cd5995fe776387b751734f4fabe478d127
Dave Walker <david.walker5@icloud.com>
2024-12-02 16:54:38 +0000

    Added 'order more' date to the output

--- src/MedicineTracker.Entities/Configuration/ApplicationSettings.cs
+++ src/MedicineTracker.Entities/Configuration/ApplicationSettings.cs
@@ -2,6 +2,7 @@ namespace MedicineTracker.Entities.Configuration
 {
     public class ApplicationSettings
     {
+        public int LeadTimeDays { get; set; }
         public int WarningDays { get; set; }
         public int CriticalDays { get; set; }
         public string DataFile { get; set; }
